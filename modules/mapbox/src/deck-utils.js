56695ac775e094c400f3d5b9e183c372a5f84ed7
Merveille KITOKO <45349907+merki230@users.noreply.github.com>
2020-09-16 20:20:30 +0100

    Update deck-utils.js
    changed deck default cursor

--- modules/mapbox/src/deck-utils.js
+++ modules/mapbox/src/deck-utils.js
@@ -214,5 +214,5 @@ function updateLayers(deck) {
     const layer = new LayerType(deckLayer.props);
     layers.push(layer);
   });
-  deck.setProps({layers});
+  deck.setProps({layers,getCursor: () => "default"});
 }
